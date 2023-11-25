ad0294d4476d72262b059f95731971d7ffd37567
Fbanqueri <Federicobanqueri@hotmail.com>
2023-11-25 18:45:54 -0300

    Update Especialidad.java

--- TrabajoIntegrador_Grupo5/src/entidades/Especialidad.java
+++ TrabajoIntegrador_Grupo5/src/entidades/Especialidad.java
@@ -1,5 +1,47 @@
 package entidades;
 
-public class Especialidad {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Especialidad implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String idEspecialidad;
+	private String nombreEspecialidad;
+
+	
+	public Especialidad(String idEspecialidad, String nombreEspecialidad) {
+		this.idEspecialidad = idEspecialidad;
+		this.nombreEspecialidad = nombreEspecialidad;
+	}
+
+	public Especialidad() {
+	}
+
+	public String getIdEspecialidad() {
+		return idEspecialidad;
+	}
+
+	public void setIdEspecialidad(String idEspecialidad) {
+		this.idEspecialidad = idEspecialidad;
+	}
+
+	public String getNombreEspecialidad() {
+		return nombreEspecialidad;
+	}
+
+	public void setNombreEspecialidad(String nombreEspecialidad) {
+		this.nombreEspecialidad = nombreEspecialidad;
+	}
+
+	@Override
+	public String toString() {
+		return "Especialidad [idEspecialidad=" + idEspecialidad + ", nombreEspecialidad=" + nombreEspecialidad + "]";
+	}
 
 }
