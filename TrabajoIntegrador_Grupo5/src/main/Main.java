2902a1b09718a2b374db943094c88a1450ed54f6
JosCapdev <josecapdevila512@gmail.com>
2023-11-25 20:07:20 -0300

    Update Main.java

--- TrabajoIntegrador_Grupo5/src/main/Main.java
+++ TrabajoIntegrador_Grupo5/src/main/Main.java
@@ -34,11 +34,11 @@ public class Main {
     	Cliente c4 = new Cliente("FrioStar","32333334","Gonzalo","Perez","Direccion_4");
     	Cliente c5 = new Cliente("ElectroH","33125223","Martin","Rodriguez","Direccion_5");
     	
-    	Especialidad esp1 = new Especialidad("0","Windows");
-    	Especialidad esp2 = new Especialidad("0","Linux");
-    	Especialidad esp3 = new Especialidad("0","Sap");
-    	Especialidad esp4 = new Especialidad("0","Tango");
-    	Especialidad esp5 = new Especialidad("0","MacOs");
+    	Especialidad esp1 = new Especialidad("Windows");
+    	Especialidad esp2 = new Especialidad("Linux");
+    	Especialidad esp3 = new Especialidad("Sap");
+    	Especialidad esp4 = new Especialidad("Tango");
+    	Especialidad esp5 = new Especialidad("MacOs");
     	
     	Tecnico t1 = new Tecnico(0,esp1,"Matias",false);
     	Tecnico t2 = new Tecnico(0,esp2,"Roxana",false);
@@ -53,17 +53,17 @@ public class Main {
     	listaTecnicos.add(t4);
     	listaTecnicos.add(t5);
     	
-    	Operador op1 = new Operador(0,"Tomas");
-    	Operador op2 = new Operador(0,"Noelia");
-    	Operador op3 = new Operador(0,"Laura");
-    	Operador op4 = new Operador(0,"Andres");
-    	Operador op5 = new Operador(0,"Lucas");
+    	Operador op1 = new Operador("Tomas");
+    	Operador op2 = new Operador("Noelia");
+    	Operador op3 = new Operador("Laura");
+    	Operador op4 = new Operador("Andres");
+    	Operador op5 = new Operador("Lucas");
     	
-    	Incidente inc1 = new Incidente(0,"Fallo Windows",false,LocalDate.now(),LocalDate.of(2023, 12, 2), c1,listaTecnicos, esp1);
-    	Incidente inc2 = new Incidente(0,"Fallo Linux",false,LocalDate.now(),LocalDate.of(2023, 12, 2), c1,listaTecnicos, esp2);
-    	Incidente inc3 = new Incidente(0,"Fallo Sap",false,LocalDate.now(),LocalDate.of(2023, 12, 2), c1,listaTecnicos, esp3);
-    	Incidente inc4 = new Incidente(0,"Fallo Tango",false,LocalDate.now(),LocalDate.of(2023, 12, 2), c1,listaTecnicos, esp4);
-    	Incidente inc5 = new Incidente(0,"Fallo MacOs",false,LocalDate.now(),LocalDate.of(2023, 12, 2), c1,listaTecnicos, esp5);
+    	Incidente inc1 = new Incidente("Fallo Windows",false,LocalDate.now(),LocalDate.of(2023, 12, 2), c1,listaTecnicos, esp1,op1);
+    	Incidente inc2 = new Incidente("Fallo Linux",false,LocalDate.now(),LocalDate.of(2023, 12, 2), c1,listaTecnicos, esp2,op2);
+    	Incidente inc3 = new Incidente("Fallo Sap",false,LocalDate.now(),LocalDate.of(2023, 12, 2), c1,listaTecnicos, op3);
+    	Incidente inc4 = new Incidente("Fallo Tango",false,LocalDate.now(),LocalDate.of(2023, 12, 2), c1,listaTecnicos, op4);
+    	Incidente inc5 = new Incidente("Fallo MacOs",false,LocalDate.now(),LocalDate.of(2023, 12, 2), c1,listaTecnicos, op5);
     	
     	
 	}
